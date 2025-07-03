2e7406507c8ff79e06de263f8f664fa192aadb79
discipulus262 <nico@webmacd.com>
2025-07-03 16:03:23 -0400

    image compatibility

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut desc = String::new();
     let mut file = fs::File::create("test.md").expect("failed to create file");
     let mut code = String::new();
+    let mut img = String::new();
     
     println!("enter the name of your project");
     io::stdin().read_line(&mut name).expect("too bad, you did something wrong");
@@ -11,11 +12,12 @@ fn main() {
     io::stdin().read_line(&mut desc).expect("too bad, you did something wrong");
     println!("enter the example code");
     io::stdin().read_line(&mut code).expect("too bad, you did something wrong");
+    println!("enter the path to an image code");
+    io::stdin().read_line(&mut img).expect("too bad, you did something wrong");
 
-
-    let finalmd = format!("# {} \n \n{} \n \nhere is some example code: \n``` \n{} \n```", name, desc, code);
+    let finalmd = format!("# {} \n \n{} \n ![Alt text]({})\nhere is some example code: \n``` \n{} \n```", name, desc, img, code);
     
-    println!("writing to test.md: {}", finalmd);
+    println!("writing to test.md: \n {}", finalmd);
     file.write_all(finalmd.as_bytes()).expect("failed to write to file");
     file.flush().expect("failed to flush file");
 
